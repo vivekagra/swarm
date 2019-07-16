5c95d5e6e430cd6a2286685cf8a0f93fcbea1061
vivekgra <vivekgr@iitk.ac.in>
2019-07-16 11:42:27 +0530

    Jarvis Controller

--- swarm_ws/src/arduino/src/voltage_controller/voltage_controller.ino
+++ swarm_ws/src/arduino/src/voltage_controller/voltage_controller.ino
@@ -2,15 +2,13 @@
  * rosserial Publisher Example
  * Prints "hello world!"
  */
-
-//#include <ros.h>
-// #include <std_msgs/Float32MultiArray.h>
+#include <ros.h>
+#include <std_msgs/Float32MultiArray.h>
 #include <Encoder.h>
+ros::NodeHandle  nh;
 
-// ros::NodeHandle  nh;
-
-// std_msgs::Float32MultiArray msg;
-//ros::Publisher encoder_fl("encoder", &msg);
+std_msgs::Float32MultiArray msg;
+ros::Publisher encoder("encoder", &msg);
 
 
 // Encoder Instances
@@ -28,24 +26,24 @@ long long oldPos = myEnc.read();
 long long newPos = myEnc.read();
 
 // rate determines whether pwm increases or decreases
-int pwm = 256;
+int pwm = 255;
 int rate = 1;
 
 // function to run a motor
 void controlMotor(int pwm,int ENA,int IN1,int IN2)
 {
-	if(pwm>=0)
-	{
-		digitalWrite(IN1,HIGH);
-		digitalWrite(IN2,LOW);
-	}
-	else
-	{
-		digitalWrite(IN1,LOW);
-		digitalWrite(IN2,HIGH);
-		pwm = -pwm;
-	}
-	analogWrite(ENA,pwm);
+  if(pwm>=0)
+  {
+    digitalWrite(IN1,HIGH);
+    digitalWrite(IN2,LOW);
+  }
+  else
+  {
+    digitalWrite(IN1,LOW);
+    digitalWrite(IN2,HIGH);
+    pwm = -pwm;
+  }
+  analogWrite(ENA,pwm);
 }
 
 
@@ -57,61 +55,38 @@ void setup()
 	pinMode(IN1,OUTPUT);
 	pinMode(IN2,OUTPUT);
 
-	// msg.data = (float *)malloc(sizeof(float)*3);
+	msg.data = (float *)malloc(sizeof(float)*3);
 
-	// msg.data_length = 3;
+	msg.data_length = 3;
 
-	// nh.initNode();
-	// nh.advertise(encoder);
+	nh.initNode();
+	nh.advertise(encoder);
 
 }
 
-
+int T = millis();
 void loop()
 {  
+  // t0 and t1 are time after which we measure a rpm
+  long t0 = millis();
+  long t1 = millis();
+  // measure rpm for every second
+  oldPos = myEnc.read();
+  newPos = myEnc.read();
+  while(t1-t0<250)
+  {
+    controlMotor(pwm, ENA, IN1, IN2);
+    newPos = myEnc.read();		
+    t1=millis();
+  }
 
-	//set pwm
-	if(pwm>=256)
-	{
-		rate = -1;
-	}
-	else if(pwm<=-1)
-	{
-		rate = 1;
-	}
-	pwm = pwm + rate;
-	
-	// T1-T0 are time for running a pwm value
-	long T0 = millis();
-	long T1 = millis();
+  // calculate rpm;
+  float rpm = (float(newPos-oldPos))*240/280;
+  // publish data
+  msg.data[0] = millis()-T;
 
-	// run motor for a pwm for 5 sec;
-	while(T1-T0<5000)
-	{
-		// t0 and t1 are time after which we measure a rpm
-		long t0 = millis();
-		long t1 = millis();
-		// measure rpm for every second
-   
-    oldPos = myEnc.read();
-    newPos = myEnc.read();
-		while(t1-t0<1000)
-		{
-			controlMotor(pwm, ENA, IN1, IN2);
-			newPos = myEnc.read();		
-			t1=millis();
-		}
-
-		// calculate rpm;
-		float rpm = (float(newPos-oldPos))*60/280;
-		float voltage = analogRead(A7); 
-		// // publish data
-		// msg.data[0] = pwm;
-
-		// msg.data[1] = rpm;
-		Serial.print(pwm);Serial.print(" -> ");Serial.print(voltage);Serial.print(" -> ");Serial.println(rpm);
-		// encoder.publish(&msg);
-		// nh.spinOnce();
-		T1 = millis();
-	}
+  msg.data[1] = rpm;
+  //Serial.print(pwm);Serial.print(" -> ");Serial.print(voltage);Serial.print(" -> ");Serial.println(rpm);
+  encoder.publish(&msg);
+  nh.spinOnce();
 }
